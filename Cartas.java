cd616fd2a67e56a8fbb38c9fc9e08906fb2b9748
AABirthday <61792725+AABirthday@users.noreply.github.com>
2021-05-22 23:34:43 -0500

    Se actualizo la clase cartas

--- Cartas.java
+++ Cartas.java
@@ -8,22 +8,3 @@ import javax.swing.JButton;
  *
  * @author Aarón
  */
-public class Cartas extends JButton implements ActionListener {
-    private int id;
-    private boolean encontrado;
-    private boolean destapado;
-    private String imagen;
-    private String imagenTapada;
-    private ImageIcon icono;
-    private ImageIcon iconoTapado;    
-
-    
-    public Cartas(int id) {        
-        this.id = id;
-        this.destapado=false;
-        this.encontrado=false;
-        this.setBackground(Color.darkGray);          
-        addActionListener(this);
-    }
-
-}
